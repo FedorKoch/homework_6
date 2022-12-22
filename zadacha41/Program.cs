af0cedf999bf1d72b2116a382c32e4d79f654171
Fedor <fedor.kochkin48@gmail.com>
2022-12-23 02:49:17 +0300

    решение задачи 41

---
+++ zadacha41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+int k = 0;
+int n = int.Parse(Console.ReadLine());
+int [] mas = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+for (int i = 0; i < mas.Length; i++)
+{ 
+    if (mas[i] > 0)
+    k = k + 1;
+}
+Console.WriteLine();
+Console.WriteLine("положительных чисел");
+Console.WriteLine(k);
